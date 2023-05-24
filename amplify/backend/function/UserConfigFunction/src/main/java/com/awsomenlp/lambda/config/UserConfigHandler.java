0564489a72478956e9e0e5581f7b06a7562e7f4b
username <k.s.caldera@student.tudelft.nl>
2023-05-24 11:29:36 +0200

    Added a hardcoded blogpost that works with AppSync

--- amplify/backend/function/UserConfigFunction/src/main/java/com/awsomenlp/lambda/config/UserConfigHandler.java
+++ amplify/backend/function/UserConfigFunction/src/main/java/com/awsomenlp/lambda/config/UserConfigHandler.java
@@ -5,6 +5,8 @@ package com.awsomenlp.lambda.config;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -50,26 +52,33 @@ public class UserConfigHandler implements RequestStreamHandler {
         //System.out.println(appSyncResolver.resolveAppSyncInput(rootNode.path("arguments"), objectMapper));
 
 
-        String jsonOut =    "{\n" +
-            "    \"id\": 100,\n"
-            +
-            "    \"blogPostLanguageCode\": \"en\",\n"
-            +
-            "    \"title\": \"Biking\",\n"
-            +
-            "    \"authors\": [\"Remi Wolf\"],\n"
-            +
-            "    \"translationModel\": {\n"
-            +
-            "        \"id\": 1,\n"
-            +
-            "        \"name\":\"newchange\"\n"
-            +
-            "    }\n"
-            +
-            "}";
+        /**
+         * refer to the foreign keys in the output for the issues to properly function
+         */
 
-        output.write(jsonOut.getBytes(StandardCharsets.UTF_8));
+        JSONObject translationModel = new JSONObject();
 
+        JSONArray authors = new JSONArray();
+        authors.put("Remi Wolf");
+        authors.put("Gutter Girl");
+
+        JSONArray content = new JSONArray();
+        content.put("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+            " Integer et ante molestie, lobortis ligula ut, iaculis lectus. Sed " +
+            "ultricies consectetur iaculis. Vestibulum venenatis mattis arcu.");
+        content.put("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum.");
+
+        JSONObject blogPost = new JSONObject();
+        blogPost.put("id", 100);
+        blogPost.put("blogPostLanguageCode", "en");
+        blogPost.put("blogPostOriginalPostId", "10");
+        blogPost.put("title", "Biking");
+        blogPost.put("authors", authors);
+        blogPost.put("blogPostTranslationModelId", "0");
+        blogPost.put("content", content);
+
+
+
+        output.write(blogPost.toString().getBytes(StandardCharsets.UTF_8));
     }
 }
