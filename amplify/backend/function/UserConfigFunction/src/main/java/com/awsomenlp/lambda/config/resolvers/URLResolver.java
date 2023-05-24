7679384c90b36a3d097bdc9f361c2274d5d665f4
Pablo Lacombe <pablo.lacombe@hotmail.com>
2023-05-24 12:44:51 +0200

    URL Resolver
    Added logic for URL resolver which turns a url into a AWS BlogPost and throws IOException if fails

--- amplify/backend/function/UserConfigFunction/src/main/java/com/awsomenlp/lambda/config/resolvers/URLResolver.java
+++ amplify/backend/function/UserConfigFunction/src/main/java/com/awsomenlp/lambda/config/resolvers/URLResolver.java
@@ -1,11 +1,14 @@
 package com.awsomenlp.lambda.config.resolvers;
 
 
-import com.awsomenlp.lambda.config.objects.AWSBlogPost;
-import com.awsomenlp.lambda.config.objects.Author;
-import com.awsomenlp.lambda.config.objects.Language;
-import com.awsomenlp.lambda.config.objects.Text;
+import com.awsomenlp.lambda.config.objects.*;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class URLResolver {
 
@@ -14,19 +17,32 @@ public class URLResolver {
     }
 
     //TODO
-    public Text resolve(String url) {
-        return new AWSBlogPost(Language.ENGLISH, "To Kill A Mockingbird",
-            List.of(new Author("", "Harper", "Lee")),
-            List.of("When he was nearly thirteen, my brother Jem got his arm badly broken at the\n" +
-            "elbow. When it healed, and Jem’s fears of never being able to play football were\n" +
-            "assuaged, he was seldom self-conscious about his injury. His left arm was\n" +
-            "somewhat shorter than his right; when he stood or walked, the back of his hand was\n" +
-            "at right angles to his body, his thumb parallel to his thigh. He couldn’t have cared\n" +
-            "less, so long as he could pass and punt.", "When enough years had gone by to enable us to look back on them, we\n" +
-            "sometimes discussed the events leading to his accident. I maintain that the Ewells\n" +
-            "started it all, but Jem, who was four years my senior, said it started long before\n" +
-            "that. He said it began the summer Dill came to us, when Dill first gave us the idea\n" +
-            "of making Boo Radley come out."));
-    }
+    public Text resolve(String url) throws IOException {
+        Document doc;
 
+        // Retrieve the HTML located at the URL
+        doc = Jsoup.connect(url).get();
+
+        //Get Title from blogpost
+        Elements titEles = doc.select("h1");
+        String title = "";
+        if (!titEles.isEmpty())
+            title = titEles.first().text();
+
+        //get Authors from blogpost
+        Elements authEles = doc.select("[property=author]");
+        List<Author> auths = new ArrayList<>();
+        for (Element ele : authEles) {
+            auths.add(new Author("", "", ele.text()));
+        }
+
+        //get headers and paragraphs from blogpost
+        Elements paraEles = doc.select("p, h2");
+        List<String> paragraphs = new ArrayList<>();
+        for (Element ele : paraEles) {
+            paragraphs.add(ele.text());
+        }
+
+        return new AWSBlogPost(Language.ENGLISH, title, auths, paragraphs);
+    }
 }
