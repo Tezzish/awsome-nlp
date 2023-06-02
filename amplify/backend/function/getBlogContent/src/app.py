ed907e361bebc7476c31dba5ecc7fd6499c5cdaf
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-02 17:20:33 +0200

    unnecessary comment dfeleted

--- amplify/backend/function/getBlogContent/src/app.py
+++ amplify/backend/function/getBlogContent/src/app.py
@@ -3,13 +3,11 @@ import bs4 as bs
 from urllib.request import urlopen
 
 def handler(event, context):
-  # url = event['arguments']['url']
+
   print('received event:')
   print(event)
   print(event['arguments'])
   response = parser(event['arguments']['url'])
-#   print(parser(event['url']))
-  # body = parser(event['url'])
 
   return {
       'statusCode': 200,
@@ -21,13 +19,12 @@ def handler(event, context):
       'file' : response
   }
 
-# # function called when the url is passed to the lambda function
+
 def parser(url):
 
-    # gets the html from the website
+  
     html = urlopen(url).read()
-    # print(html)
-
+ 
     soup = bs.BeautifulSoup(html, 'html.parser')
     title = soup.find('h1', class_='lb-h2 blog-post-title')
     content = soup.find('section', class_='blog-post-content lb-rtxt')
@@ -35,7 +32,7 @@ def parser(url):
     paragraphList = []
     for i in paragraphs:
         paragraphList.append(str(i))
-    # print(title.text + content.text + '\n'.join(paragraphList))
+
     beginning = '''<!DOCTYPE html>
                   <html>
                   <head>
@@ -47,5 +44,3 @@ def parser(url):
     
     
     
-# parser("https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el")
-# print(handler({'arguments': {'url': 'https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el'}}, 1))
