5989dbbebf3ec7b8bcad2405128e99b371831d75
Ishan <ishanpahwa14@gmail.com>
2023-06-02 11:45:28 +0200

    Adding basic frontend tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+//tests for the URL input
+test('URL Input', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const app = screen.getByPlaceholderText('AWS Blogpost (URL)');
+  expect(app).toBeInTheDocument();
 });
+
+//tests for the language input
+test('Language Input', () => {
+  render(<App />);
+  const app = screen.getByPlaceholderText('Target Language');
+  expect(app).toBeInTheDocument();
+});
+
+//tests for the translate button
+test('Translate Button', () => {
+  render(<App />);
+  const app = screen.getByText('Translate!');
+  //checks if it's in the document
+  expect(app).toBeInTheDocument();
+  //checks if it's enabled
+  expect(app).toBeEnabled();
+  //checks if it's a button
+  expect(app).toBeInstanceOf(HTMLButtonElement);
+});
+
