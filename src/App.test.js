4f4654c6be9f91a3e37b4861162f37a7af06c345
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-02 11:58:34 +0200

    tests if valid url works

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
+
+// tests if the valid url method works
+test('check if valid url is valid', () => {
+  expect(isValidURL("https://aws.amazon.com/blogs/")).toBe(true);
+}
+);
