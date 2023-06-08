4f4cd518394a9a4c3a1306a2dac5feb4a70508d7
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-08 17:34:56 +0200

    conflict fixes

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -5,11 +5,6 @@
  **************************************************************************/
 
 export { default as studioTheme } from "./studioTheme";
-<<<<<<< HEAD
-=======
-export { default as TranslationConfigCreateForm } from "./TranslationConfigCreateForm";
-export { default as TranslationConfigUpdateForm } from "./TranslationConfigUpdateForm";
->>>>>>> f030128dc26b983d960594080b7cc882eb83df9d
 export { default as LanguageCreateForm } from "./LanguageCreateForm";
 export { default as LanguageUpdateForm } from "./LanguageUpdateForm";
 export { default as TranslationModelCreateForm } from "./TranslationModelCreateForm";
