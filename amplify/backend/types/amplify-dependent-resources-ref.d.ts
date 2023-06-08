f8d2ae234ea7dfbfca2a617eb8acd808a5e6df35
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-08 18:58:33 +0200

    fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -21,13 +21,6 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "checkingDatabase": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "getBlogContent": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
