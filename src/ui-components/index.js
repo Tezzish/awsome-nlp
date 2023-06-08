d429019a15a0522af018728bf689a5cc46cf87ad
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-08 17:37:08 +0200

    merge fix

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -5,11 +5,6 @@
  **************************************************************************/
 
 export { default as studioTheme } from "./studioTheme";
-<<<<<<< HEAD
-=======
-export { default as TranslationConfigCreateForm } from "./TranslationConfigCreateForm";
-export { default as TranslationConfigUpdateForm } from "./TranslationConfigUpdateForm";
->>>>>>> dafb4ab1e492584782d1f095670ba317676dd344
 export { default as LanguageCreateForm } from "./LanguageCreateForm";
 export { default as LanguageUpdateForm } from "./LanguageUpdateForm";
 export { default as TranslationModelCreateForm } from "./TranslationModelCreateForm";
