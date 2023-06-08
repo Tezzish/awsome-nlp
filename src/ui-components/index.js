8f3d786394562c92f15fa6f5787ea51443416500
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2023-06-08 17:25:12 +0200

    working environment + new lambda template

---
+++ src/ui-components/index.js
@@ -0,0 +1,13 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+export { default as studioTheme } from "./studioTheme";
+export { default as TranslationConfigCreateForm } from "./TranslationConfigCreateForm";
+export { default as TranslationConfigUpdateForm } from "./TranslationConfigUpdateForm";
+export { default as LanguageCreateForm } from "./LanguageCreateForm";
+export { default as LanguageUpdateForm } from "./LanguageUpdateForm";
+export { default as TranslationModelCreateForm } from "./TranslationModelCreateForm";
+export { default as TranslationModelUpdateForm } from "./TranslationModelUpdateForm";
