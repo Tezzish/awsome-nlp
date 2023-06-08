2586829ba56820ae2463ef10c9444ddd7bc4c9c5
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-08 16:14:28 +0200

    trying my best

--- amplify/backend/function/getBlogContent/src/app.py
+++ amplify/backend/function/getBlogContent/src/app.py
@@ -1,30 +1,17 @@
-
-
 import json
 import bs4 as bs
-import requests
 from urllib.request import urlopen
 
 def handler(event, context):
-    # check if the url sends a 404 response code
+  # url = event['arguments']['url']
+  print('received event:')
+  print(event)
+  print(event['arguments'])
+  response = parser(event['arguments']['url'])
+#   print(parser(event['url']))
+  # body = parser(event['url'])
 
-    # the url to check
-    url = event['arguments']['url']
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            # if page exists, and has correct response continue
-            print("Page exists")
-        else:
-            # if page doesn't exis
-            return False
-    except requests.exceptions.RequestException as e:
-        # if an error occurs
-        print("Error checking if URL exists:", e)
-        return False  
-    
-    response = parser(url)
-    return {
+  return {
       'statusCode': 200,
       'headers': {
           'Access-Control-Allow-Headers': '*',
@@ -34,12 +21,13 @@ def handler(event, context):
       'file' : response
   }
 
-
+# # function called when the url is passed to the lambda function
 def parser(url):
 
-  
+    # gets the html from the website
     html = urlopen(url).read()
- 
+    # print(html)
+
     soup = bs.BeautifulSoup(html, 'html.parser')
     title = soup.find('h1', class_='lb-h2 blog-post-title')
     content = soup.find('section', class_='blog-post-content lb-rtxt')
@@ -47,7 +35,7 @@ def parser(url):
     paragraphList = []
     for i in paragraphs:
         paragraphList.append(str(i))
-
+    # print(title.text + content.text + '\n'.join(paragraphList))
     beginning = '''<!DOCTYPE html>
                   <html>
                   <head>
@@ -58,6 +46,9 @@ def parser(url):
     return (beginning + str(title) + str(content) + '\n'.join(paragraphList) + '</body></html>')
     
     
+    
+# parser("https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el")
+# print(handler({'arguments': {'url': 'https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el'}}, 1))
     
 
 # import json
