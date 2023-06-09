c30252edb8ce5299d7ccad66ef95b1325bfc0cd6
larasakarya <72052108+larasakarya@users.noreply.github.com>
2023-06-09 17:43:11 +0200

    LHS working (#26)

--- amplify/backend/function/getBlogContent/src/app.py
+++ amplify/backend/function/getBlogContent/src/app.py
@@ -1,15 +1,26 @@
 import json
 import bs4 as bs
+import urllib.request
 from urllib.request import urlopen
 
 def handler(event, context):
-  # url = event['arguments']['url']
-  print('received event:')
-  print(event)
-  print(event['arguments'])
-  response = parser(event['arguments']['url'])
-#   print(parser(event['url']))
-  # body = parser(event['url'])
+  blogUrl = event['arguments']['url']
+  
+  try:
+    response = urllib.request.urlopen(blogUrl)
+    if response.status != 404:
+      # if page exists, and has correct response continue
+      print("Page exists")
+    else:
+      # if page doesn't exist
+        return False
+  except:
+      # if an error occurs
+      print("Error checking if URL exists:")
+      return False
+
+  
+  postReturned = parser(blogUrl)
 
   return {
       'statusCode': 200,
@@ -18,34 +29,13 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'file' : response
+      'file' : postReturned
   }
 
-# # function called when the url is passed to the lambda function
 def parser(url):
-
-    # gets the html from the website
     html = urlopen(url).read()
-    # print(html)
-
     soup = bs.BeautifulSoup(html, 'html.parser')
-    title = soup.find('h1', class_='lb-h2 blog-post-title')
-    content = soup.find('section', class_='blog-post-content lb-rtxt')
-    paragraphs = soup.find_all('p')
-    paragraphList = []
-    for i in paragraphs:
-        paragraphList.append(str(i))
-    # print(title.text + content.text + '\n'.join(paragraphList))
-    beginning = '''<!DOCTYPE html>
-                  <html>
-                  <head>
-                  </head>
-                  <body>
-                  '''
+    blog_content = soup.find('div', class_='aws-blog-content').prettify()
+    return blog_content
 
-    return (beginning + str(title) + str(content) + '\n'.join(paragraphList) + '</body></html>')
-    
-    
-    
-# parser("https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el")
-# print(handler({'arguments': {'url': 'https://aws.amazon.com/blogs/aws/new-amazon-aurora-i-o-optimized-cluster-configuration-with-up-to-40-cost-savings-for-i-o-intensive-applications/?trk=f06df17d-71cb-481d-b7b8-8dd14f9b578c&sc_channel=el'}}, 1))
+#print(handler({'arguments': {'url': 'https://aws.amazon.com/blogs/devops/optimize-software-development-with-amazon-codewhisperer/?trk=7a3f46e1-9c31-4493-9807-8174ec76e623&sc_channel=el'}}, None))
