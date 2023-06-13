eaa951b8271369fab54db98e013895c6b2228dcc
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2023-06-13 12:34:54 +0200

    working lambda

--- amplify/backend/function/checkingUrl/src/index.js
+++ amplify/backend/function/checkingUrl/src/index.js
@@ -8,9 +8,10 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
   var segments = event['url'].split('/');
+  console.log(event['url'])
   var lastSegment = segments[segments.length - 1];
   const params = {
-    TableName : 'translation-storages-db',
+    TableName : 'translations-aws-blog-posts',
     /* Item properties will depend on your application concerns */
     Key: {
       URL: lastSegment
@@ -24,6 +25,23 @@ exports.handler = async (event, context) => {
      };
   }
   else {
+  //   // Specify the bucket name
+  // const bucketName = data['Item']['RHS_s3_loc'];
+
+  // // Specify the file names you want to retrieve
+  // const fileNames = ['file1.txt', 'file2.txt', 'file3.txt'];
+
+  // try {
+    // const promises = fileNames.map(async (fileName) => {
+    //   // Retrieve the contents of each file
+    //   const response = await s3.getObject({ Bucket: bucketName, Key: fileName }).promise();
+    //   const content = response.Body.toString('utf-8');
+
+    //   return { fileName, content };
+    // });
+
+    // // Wait for all promises to resolve
+    // const results = await Promise.all(promises);
   return {urlPresent: true,
          body: JSON.stringify(data)};
   }
