3d46336abe679c63db0afea8e61ec8a1f7f30a4f
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2023-06-13 12:32:25 +0200

    working lambda

--- amplify/backend/function/checkingUrl/src/index.js
+++ amplify/backend/function/checkingUrl/src/index.js
@@ -1,5 +1,3 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
@@ -9,18 +7,24 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
+  var segments = event['url'].split('/');
+  var lastSegment = segments[segments.length - 1];
   const params = {
-    TableName : 'translations-aws-blog-posts',
+    TableName : 'translation-storages-db',
     /* Item properties will depend on your application concerns */
     Key: {
-      URL: "?trk=7a3f46e1-9c31-4493-9807-8174ec76e623&sc_channel=el"
+      URL: lastSegment
     }
+  };
+  const data = await docClient.get(params).promise();
+  console.log(data);
+  if (JSON.stringify(data) == "{}"){
+     return {urlPresent: false,
+       url: event['url']
+     };
   }
-  try {
-    const data = await docClient.get(params).promise()
-    print(data)
-    return { body: JSON.stringify(data) }
-  } catch (err) {
-    return { error: err }
+  else {
+  return {urlPresent: true,
+         body: JSON.stringify(data)};
   }
-}
+};
