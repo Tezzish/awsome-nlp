25fea39890fd44126c84a52624615cd79238aec3
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-13 16:03:31 +0200

    accidental lambda deletion fixed

--- amplify/backend/function/stepFunctionInvoker/src/index.py
+++ amplify/backend/function/stepFunctionInvoker/src/index.py
@@ -7,7 +7,6 @@ client = boto3.client('stepfunctions')
 # function to invoke step function state machine with url as input
 def handler(event, context):
 
-
     response = client.start_execution(
     stateMachineArn='arn:aws:states:eu-west-1:755811905719:stateMachine:applicationWorkflow',
     input = json.dumps({'url' : event['url']})
