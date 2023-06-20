2e0d5c92cba175999db45d531827119c4f71b8de
username <k.s.caldera@student.tudelft.nl>
2023-06-20 20:35:58 +0200

    Added a build script to export GetBlogContent function

---
+++ amplify/backend/function/getBlogContent/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p package
+
+
+pip3 install --target package -r Requirements.txt
+cp -r src/* package
+
+cd package
+zip -r get_blog_content.zip .
+
+
+mv get_blog_content.zip ../
+
+
+cd ..
+rm -r package
+\echo "Deployment package get_blog_content.zip has been created"
