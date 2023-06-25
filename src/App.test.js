fc531bc699efc05ddc6f34c3a748fb48cf099a25
Lara Sakarya <L.Sakarya@student.tudelft.nl>
2023-06-25 22:00:15 +0200

    frontend testing finalized

--- src/App.test.js
+++ src/App.test.js
@@ -23,178 +23,21 @@ jest.mock('aws-amplify', () => ({
 }));
 
 
+// test the isValidURL function
+describe('isValidURL', () => {
+  test('returns true for valid URLs', () => {
+    expect(isValidURL('https://aws.amazon.com/blogs/aws/learn-how-to-streamline-and-secure-your-saas-applications-at-aws-applications-innovation-day/?trk=b34102c3-e32b-4e8b-8ff6-a5c374bbc252&sc_channel=ele')).toBe(true);
+  });
+});
 
-// // test the isValidURL function
-// describe('isValidURL', () => {
-//   test('returns true for valid URLs', () => {
-//     expect(isValidURL('https://aws.amazon.com/blogs/aws/learn-how-to-streamline-and-secure-your-saas-applications-at-aws-applications-innovation-day/?trk=b34102c3-e32b-4e8b-8ff6-a5c374bbc252&sc_channel=ele')).toBe(true);
-//   });
-// });
-
-// // test the isValidURL function
-// describe('isValidURL', () => {
-//   test('returns false for invalid URLs', () => {
-//     expect(isValidURL('https://en.wikipedia.org/wiki/Turkey')).toBe(false);
-//   });
-// });
+// test the isValidURL function
+describe('isValidURL', () => {
+  test('returns false for invalid URLs', () => {
+    expect(isValidURL('https://en.wikipedia.org/wiki/Turkey')).toBe(false);
+  });
+});
 
 describe('App', () => {
-
-  // clear the mocks before each test
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });   
-
-  // test('should call sendOriginalAndTranslated and its internal functions', async () => {
-  //   // Mock the required dependencies and set initial values
-  //   const url = 'https://aws.amazon.com/blogs/aws/learn-how-to-streamline-and-secure-your-saas-applications-at-aws-applications-innovation-day/?trk=b34102c3-e32b-4e8b-8ff6-a5c374bbc252&sc_channel=el';
-  //   const sourceLanguage = { label: 'ENGLISH', value: 'en' };
-  //   const targetLanguage = { label: 'TURKISH', value: 'tr' };
-  //   const translationModel = { label: 'amazonTranslate', value: 'amazonTranslate' };
-
-  //   // Mock the internal functions
-  //   const setRatingMock = jest.fn();
-  //   const setRatingSubmittedMock = jest.fn();
-  //   const setOriginalPostMock = jest.fn();
-  //   const setTranslatedPostMock = jest.fn();
-  //   const setIsLoadingMock = jest.fn();
-  //   const setBackendFinishedMock = jest.fn();
-  //   const setRatingBlogPostIdMock = jest.fn();
-
-  //   // Mock the API.graphql function
-  //   const mockedGraphql = jest.fn().mockResolvedValue({
-  //     data: {
-  //       getStepFunctionInvoker: {
-  //         lhs: '<p>Original post content</p>',
-  //         rhs: '<p>Translated post content</p>',
-  //         id: 'ratingBlogPostId',
-  //       },
-  //     },
-  //   });
-
-  //   // Mock the API object
-  //   const mockedAPI = {
-  //     graphql: mockedGraphql,
-  //   };
-
-  //   // Render the App component
-  //   render(<App />);
-
-  //   // Mock the useState hook functions
-  //   jest.spyOn(React, 'useState').mockImplementation((initialValue) => {
-  //     if (initialValue === 0) return [0, setRatingMock];
-  //     if (initialValue === false) return [false, setRatingSubmittedMock];
-  //     if (initialValue === '') return ['', setOriginalPostMock];
-  //     if (initialValue === '') return ['', setTranslatedPostMock];
-  //     if (initialValue === true) return [true, setIsLoadingMock];
-  //     if (initialValue === false) return [false, setBackendFinishedMock];
-  //     if (initialValue === '') return ['', setRatingBlogPostIdMock];
-  //     return jest.requireActual('react').useState(initialValue);
-  //   });
-
-  //   // Call the function
-  //   await sendOriginalAndTranslated(url, sourceLanguage, targetLanguage, translationModel);
-
-  //   // Assertions
-  //   expect(console.log).toHaveBeenCalledWith('sending config to backend');
-  //   expect(console.log).toHaveBeenCalledWith('target language: ' + targetLanguage);
-  //   expect(console.log).toHaveBeenCalledWith(targetLanguage);
-  //   expect(console.log).toHaveBeenCalledWith('translation model: ' + translationModel);
-  //   expect(console.log).toHaveBeenCalledWith(translationModel);
-  //   expect(mockedGraphql).toHaveBeenCalledTimes(1);
-  //   expect(mockedGraphql).toHaveBeenCalledWith({
-  //     input: {
-  //       url: url,
-  //       sourceLanguage: { name: 'ENGLISH', code: 'en' },
-  //       targetLanguage: { name: targetLanguage.label, code: targetLanguage.value },
-  //       translationModel: { type: translationModel.label },
-  //     },
-  //   });
-  //   expect(console.log).toHaveBeenCalledWith('send successful');
-  //   expect(console.log).toHaveBeenCalledWith(JSON.stringify({ data: { getStepFunctionInvoker: { lhs: '<p>Original post content</p>', rhs: '<p>Translated post content</p>', id: 'ratingBlogPostId' } } }));
-  //   expect(setRatingMock).toHaveBeenCalledWith(0);
-  //   expect(setRatingSubmittedMock).toHaveBeenCalledWith(false);
-  //   expect(setOriginalPostMock).toHaveBeenCalledWith('<p>Original post content</p>');
-  //   expect(setTranslatedPostMock).toHaveBeenCalledWith('<p>Translated post content</p>');
-  //   expect(setIsLoadingMock).toHaveBeenCalledWith(false);
-  //   expect(setBackendFinishedMock).toHaveBeenCalledWith(true);
-  //   expect(setRatingBlogPostIdMock).toHaveBeenCalledWith('ratingBlogPostId');
-  //   expect(console.error).not.toHaveBeenCalled();
-  //   expect(setIsLoadingMock).toHaveBeenCalledWith(false);
-  // });
-
-  test('renders the app and translates the post', async () => {
-    const mockedAPI = API;
-
-    // Mock the API.graphql function
-    mockedAPI.graphql.mockResolvedValue({
-      data: {
-        getStepFunctionInvoker: {
-          lhs: '<p>Original post content</p>',
-          rhs: '<p>Translated post content</p>',
-          id: 'ratingBlogPostId',
-        },
-      },
-    });
-
-    render(<App />);
-
-    // Simulate user input and button click
-    const inputElement = screen.getByTestId('url-input');
-    console.log('THE INPUT ELEMENT');
-    console.log(inputElement);
-    fireEvent.change(inputElement, {
-    target: { value: 'https://aws.amazon.com/blogs/aws/learn-how-to-streamline-and-secure-your-saas-applications-at-aws-applications-innovation-day/?trk=b34102c3-e32b-4e8b-8ff6-a5c374bbc252&sc_channel=ele'},
-  });
-  console.log('THE INPUT ELEMENT 2');
-    console.log(inputElement);
-    
-    // Simulate change event on language select
-
-    // get the element with id select-language
-    const languageSelect = screen.getByTestId('select-language')
-    fireEvent.change(languageSelect, {
-      target: { value: { label: 'TURKISH', value: 'tr' } },
-    });
-
-    // Simulate change event on model select
-    const modelSelect = screen.getByTestId('translation-model-select')
-    fireEvent.change(modelSelect, {
-      target: { value: { label: 'amazonTranslate', value: 'amazonTranslate' } },
-    });
-
-    fireEvent.click(screen.getByText('Translate!'));
-
-    // Wait for the API call to finish
-    await waitFor(() => expect(mockedAPI.graphql).toHaveBeenCalledTimes(2));
-
-    // get the html element with id leftSide and rightSide
-    const leftSide = await waitFor(() => screen.getByLabelText('leftSide'));
-    const rightSide = await waitFor(() => screen.getByLabelText('rightSide'));
-
-    // Assertions
-    expect(mockedAPI.graphql).toHaveBeenCalledTimes(2);
-    expect(mockedAPI.graphql).toHaveBeenCalledWith(
-      graphqlOperation(getStepFunctionInvoker, {
-        input: {
-          url: "https://aws.amazon.com/blogs/aws/learn-how-to-streamline-and-secure-your-saas-applications-at-aws-applications-innovation-day/?trk=b34102c3-e32b-4e8b-8ff6-a5c374bbc252&sc_channel=el",
-          sourceLanguage: { name: 'ENGLISH', code: 'en' },
-          targetLanguage: { name: 'TURKISH', code: 'tr' },
-          translationModel: { type: 'amazonTranslate' },
-        },
-      })
-    );
-
-    expect(leftSide).toBeInTheDocument();
-    expect(rightSide).toBeInTheDocument();
-    // expect right side to have Translated post content
-    expect(rightSide.innerHTML).toBe('<p>Translated post content</p>');
-    // expect left side to have Original post content
-    expect(leftSide.innerHTML).toBe('<p>Original post content</p>');
-    
-  });
-
-
 
   test('renders App component', async () => {
     render(<App />);
@@ -206,21 +49,21 @@ describe('App', () => {
     expect(screen.getByText('Translate!')).toBeInTheDocument();
   });
 
-  // test('handleButtonClick for invalid URL', async () => {
-  //   render(<App />);
+  test('handleButtonClick for invalid URL', async () => {
+    render(<App />);
 
-  //   // Clicking the button without any data should call alert with 'Incorrect Link' error
-  //   fireEvent.click(screen.getByText('Translate!'));
-  //   await waitFor(() => screen.getByText('Incorrect Link'));
-  // });
+    // Clicking the button without any data should call alert with 'Incorrect Link' error
+    fireEvent.click(screen.getByText('Translate!'));
+    await waitFor(() => screen.getByText('Incorrect Link'));
+  });
 
-  // test('clicking button with no data given', async () => {
-  //   render(<App />);
+  test('clicking button with no data given', async () => {
+    render(<App />);
 
-  //   // Clicking the button without any data should call alert with 'Incorrect Link' error
-  //   fireEvent.click(screen.getByText('Translate!'));
-  //   await waitFor(() => screen.getByText('Incorrect Link'));
-  // });
+    // Clicking the button without any data should call alert with 'Incorrect Link' error
+    fireEvent.click(screen.getByText('Translate!'));
+    await waitFor(() => screen.getByText('Incorrect Link'));
+  });
 
  
 });
