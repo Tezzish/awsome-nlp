1a2c42ff3777ac6f590e3ade62a24926b00f21a6
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2023-06-27 00:57:00 +0200

    testing working

--- amplify/backend/function/storingTranslation/test/test_module.py
+++ amplify/backend/function/storingTranslation/test/test_module.py
@@ -17,7 +17,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 src_dir = os.path.join(parent_dir, 'src')
 sys.path.append(src_dir)
 
-import index
+
 
 @pytest.fixture
 def lhs_content():
@@ -36,33 +36,47 @@ def rhs_content():
     }
 
 
+# @pytest.fixture(autouse=True)
+# def mock_translation_table_name(monkeypatch):
+#     translation_table_name = os.getenv('TRANSLATION_TABLE_NAME', 'default_table')
+#     monkeypatch.setenv('TRANSLATION_TABLE_NAME', translation_table_name)
+
+
 # Test if the function correctly replaces the title
-def test_replace_text_with_translation_title(lhs_content, rhs_content):
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+def test_replace_text_with_translation_title(lhs_content, rhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
 
     assert soup.find('h1', class_="lb-h2 blog-post-title").string == 'Translated Title'
 
 
 # Test if the function correctly replaces the author
-def test_replace_text_with_translation_author(lhs_content, rhs_content):
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+def test_replace_text_with_translation_author(lhs_content, rhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
 
     assert soup.find('p', property="name").string == 'Translated Author'
 
 
 # Test if the function correctly replaces the content
-def test_replace_text_with_translation_content(lhs_content, rhs_content):
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+def test_replace_text_with_translation_content(lhs_content, rhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
 
     assert soup.find('h2', class_="lb-h5 blog-title").string == 'Translated Content'
 
 
 # Test if the function correctly replaces the Title, Author and Content
-def test_replace_text_with_translation(lhs_content, rhs_content):
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+def test_replace_text_with_translation(lhs_content, rhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
 
     assert soup.find('h1', class_="lb-h2 blog-post-title").string == 'Translated Title'
@@ -71,53 +85,63 @@ def test_replace_text_with_translation(lhs_content, rhs_content):
 
 
 # Test if the function correctly replaces Author and Content without Title
-def test_no_title_in_rhs_content(lhs_content):
+def test_no_title_in_rhs_content(lhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     rhs_content = {
         'author': ['Translated Author'],
         'content': ['Translated Content']
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert soup.find('h1', class_="lb-h2 blog-post-title").string == 'Title'  # unchanged title
 
 
 # Test if the function correctly replaces Title and Content without Author
-def test_no_author_in_rhs_content(lhs_content):
+def test_no_author_in_rhs_content(lhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     rhs_content = {
         'title': 'Translated Title',
         'content': ['Translated Content']
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert soup.find('p', property="name").string == 'Author'  # unchanged author
 
 
 # Test if the function correctly replaces Title and Author without Content
-def test_no_content_in_rhs_content(lhs_content):
+def test_no_content_in_rhs_content(lhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     rhs_content = {
         'title': 'Translated Title',
         'author': ['Translated Author']
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert soup.find('h2', class_="lb-h5 blog-title").string == 'Blog Title'  # unchanged content
 
 
 # Test if the function handles the case of a mismatch in the length of authors
-def test_author_length_mismatch(lhs_content):
+def test_author_length_mismatch(lhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     rhs_content = {
         'title': 'Translated Title',
         'author': ['Translated Author1', 'Translated Author2'],  # more authors than in the HTML
         'content': ['Translated Content']
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert soup.find('p', property="name").string == 'Author'  # unchanged author
 
 
 # Test if the function correctly replaces text within paragraphs that don't contain an image
-def test_replace_text_with_translation_img_paragraph(lhs_content, rhs_content):
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+def test_replace_text_with_translation_img_paragraph(lhs_content, rhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
 
     # Test if the function correctly replaces the title
@@ -131,19 +155,23 @@ def test_replace_text_with_translation_img_paragraph(lhs_content, rhs_content):
 
 
 # Test if the function handles the case of no content in the rhs_content
-def test_no_first_content_in_rhs_content(lhs_content):
+def test_no_first_content_in_rhs_content(lhs_content, monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     rhs_content = {
         'title': 'Translated Title',
         'author': ['Translated Author'],
         'content': []  # empty content
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert soup.find('h2', class_="lb-h5 blog-title").string == 'Blog Title'  # unchanged h2 blog title
 
 
 # Test if the function handles the case of no content section in the lhs_content
-def test_no_content_section_in_lhs_content():
+def test_no_content_section_in_lhs_content(monkeypatch):
+    monkeypatch.setattr('os.getenv', MagicMock(return_value='default_table'))
+    from index import replace_text_with_translation
     lhs_content = {
         'file': '<html><body><h1 class="lb-h2 blog-post-title">Title</h1><p property="name">Author</p><h2 class="lb-h5 blog-title">Blog Title</h2></body></html>',
     }  # No blog-post-content lb-rtxt in HTML
@@ -152,6 +180,6 @@ def test_no_content_section_in_lhs_content():
         'author': ['Translated Author'],
         'content': ['Translated Content']
     }
-    result = index.replace_text_with_translation(lhs_content, rhs_content)
+    result = replace_text_with_translation(lhs_content, rhs_content)
     soup = BeautifulSoup(result['file'], 'html.parser')
     assert not soup.find(class_="blog-post-content lb-rtxt", property="articleBody")  # No blog-post-content lb-rtxt in HTML
